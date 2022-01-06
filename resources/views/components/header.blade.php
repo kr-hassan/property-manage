45c8c12d90b89d8ba1233e14544a8c0a28a65167
Kazi Rabbi Hassan <kazirabbi1000@gmail.com>
2022-01-06 16:00:50 +0600

    Slider Modification

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,15 @@
-<div class="fixed w-full py-4 px-12 display-f justify-between items-center">
+<div class="fixed w-full py-4 px-12 flex justify-between items-center">
     <div class="min-w-max">
         <img width="100" src="/img/logo.png" alt="" >
     </div>
-    <div class="min-w-max text-xl">
+    <div class="w-full">
+        <ul class="flex justify-center">
+           <li><a class="inline-block p-2" href="">Land</a></li>
+           <li><a class="inline-block p-2" href="">Villa</a></li>
+        </ul>
+    </div>
+
+    <div class="min-w-max text-3xl">
         <a href="#">🇺🇸</a>
         <a href="#">🇧🇩</a>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,9 @@
 <x-guest-layout>
-    test
+  <div class="relative z-10 pt-20 pb-24 bg-cover bg-center" style="background-image: url(/img/hero-bg.jpg)">
+<div class="absolute h-full w-full bg-black opacity-30 top-0 left-0"></div>
+      <div class="container mx-auto">
+          <h2>Lorem ipsum dolor sit amet,<br> consectetur adipisicing elit. Commodi, illo.</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, quaerat?</p>
+      </div>
+  </div>
 </x-guest-layout>
