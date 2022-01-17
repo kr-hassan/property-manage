fe892df059dd770916f601d41058e58b193651ee
Kazi Rabbi Hassan <kazirabbi1000@gmail.com>
2022-01-17 17:57:27 +0600

    frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,4 +6,21 @@
             <p class="text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, quaerat?</p>
         </div>
     </div>
+    <div class="py-32 text-center">
+        <div class="container">
+            <h2 class="font-bold text-4xl text-2xl mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br/>Alias aliquam corporis <span
+                    class="underline"> See Deals</span></h2>
+            <p class="text-gray-500 font-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium architecto commodi debitis eaque eum
+                excepturi
+                exercitationem facilis inventore magni, nam, nihil nisi nobis porro ratione rem tenetur ullam voluptatum.</p>
+            <a href="" class="border-2 border-gray-500 rounded-2xl text-xl px-8 py-3 inline-block">Start The Searching</a>
+
+            <h2 class="font-bold text-4xl text-2xl mb-6 mt-12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br/>porro ratione rem
+                tenetur ullam voluptatum Alias aliquam corporis
+                <span
+                    class="underline"> CheckOut </span></h2>
+
+        </div>
+
+    </div>
 </x-guest-layout>
