41c8fe211387090f4e29ad8c7c1c2c0db4825fd3
Kazi Rabbi Hassan <kazirabbi1000@gmail.com>
2022-01-17 09:59:25 +0600

    corrpt fix

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,66 @@
+<div class="container pt-4">
+    <div class="pt-20">
+        <h2 class="section-heading"> Articles & Advices</h2>
+        <div class="flex -mx-4">
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width: 130px" class="bg-gray-500 w-64"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+                        <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width: 130px" class="bg-gray-500 w-64"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+                        <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width: 130px" class="bg-gray-500 w-64"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+                        <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="pt-20 text-lg border-b-2 border ">
+        <h2 class="section-heading"> Additional Services</h2>
+        <div class="flex -mx-4">
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width: 130px" class="bg-gray-500 w-64"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+                        <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width: 130px" class="bg-gray-500 w-64"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+                        <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-1 px-4">
+                <div class="bg-white rounded-xl flex">
+                    <div style="min-width: 130px" class="bg-gray-500 w-64"></div>
+                    <div class="p-4">
+                        <h3 class="mb-4 text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+                        <a href="" class="fullwidth-btn">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<x-guest-layout>
+    <div class="relative z-10 pt-52 pb-64 bg-cover bg-center" style="background-image: url(/img/hero-bg.jpg)">
+        <div class="absolute h-full w-full bg-black opacity-70 top-0 left-0 z-10"></div>
+        <div class="container relative z-20 text-white text-center">
+            <h2 class="font-bold text-5xl mb-8">Lorem ipsum dolor sit amet,<br> consectetur adipisicing elit. Commodi, illo.</h2>
+            <p class="text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, quaerat?</p>
+        </div>
+    </div>
+</x-guest-layout>
